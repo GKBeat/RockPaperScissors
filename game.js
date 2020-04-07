d1879b32897d5c5ec04a04577ebb4c39b57b2d9e
GoerkemYalcinkaya <goerkem.yalcinkaya@gmx.de>
2020-04-07 16:28:55 +0200

    added different Difficulties

--- game.js
+++ game.js
@@ -6,10 +6,16 @@ var currentlyFighting = false;
 var playerPoints = 0;
 var pcPoints = 0;
 
+var difficulty = "easy";
+
+var playerPattern= [];
+
 $(".button").on("click", function(event) {
     if (!currentlyFighting) {
         currentlyFighting = true;
         let playerChoice = event.target.id;
+
+        playerPattern.push(playerChoice);
 
         $(`#${playerChoice}`).addClass("pressed");
         setTimeout(function() {
@@ -19,13 +25,19 @@ $(".button").on("click", function(event) {
         player.removeClass("fa-question");
         player.addClass(`fa-hand-${playerChoice}`);
 
-        let pcChoice = pcRandomAI();
+        let pcChoice = "";
 
+        if (difficulty === "easy") {
+            pcChoice = pcEasyAI();
+
+        } else if (difficulty === "normal" || difficulty === "hard") {
+            pcChoice = pcNormalHardAI(difficulty);
+
+        } else if (difficulty === "unbeatable") {
+            pcChoice = pcUnbeatableAI(playerChoice);
+        }
 
         gameLogic(playerChoice, pcChoice);
-
-
-        currentlyFighting = false;
 
         setTimeout(function() {
             player.removeClass(`fa-hand-${playerChoice}`);
@@ -40,17 +52,104 @@ $(".button").on("click", function(event) {
             $(".pcPoints").text(`PC:${pcPoints}`)
 
         }, 1000);
+
+        currentlyFighting = false;
     }
 });
 
-function pcRandomAI() {
+$(".selectDifficulty").on("change", function(event) {
+    let selectedDifficulty = event.target.value;
+    difficulty = selectedDifficulty;
+});
+
+function pcEasyAI() {
     let randomSign = Math.floor(Math.random() * 3);
     let pcChoice = pcAnswer[randomSign];
 
-    pc.removeClass("fa-question");
-    pc.addClass(`fa-hand-${pcChoice}`);
+    changePcIcon(pcChoice);
 
     return pcChoice;
+}
+
+function pcNormalHardAI(difficulty) {
+
+    let rock = 0;
+    let paper = 0;
+    let scissors = 0;
+
+    let pcChoice = "";
+    let lastChoices = "";
+
+    if(difficulty === "normal"){
+        lastChoices = lastMultiple(3);
+
+    }else if(difficulty === "hard"){
+        lastChoices = lastMultiple(8);
+    }
+
+
+    for(choice of lastChoices){
+        if (choice === "rock") {
+            rock += 1;
+        } else if (choice === "paper") {
+            paper += 1;
+        } else if(choice === "scissors"){
+            scissors += 1;
+        }else{
+            continue;
+        }
+    }
+
+    console.log(lastChoices);
+
+    if(lastChoices[3] === undefined){
+        pcChoice = pcEasyAI();
+
+    }else if (rock >= paper && rock >= scissors) {
+        pcChoice = "paper";
+
+    } else if (paper >= rock && paper >= scissors) {
+        pcChoice = "scissors";
+
+    } else if (scissors >= rock && scissors >= scissors) {
+        pcChoice = "rock";
+
+    }
+
+    changePcIcon(pcChoice);
+
+    return pcChoice;
+}
+
+function pcUnbeatableAI(playerChoice) {
+    let pcChoice = ""
+    if (playerChoice === "rock") {
+        pcChoice = "paper";
+    } else if (playerChoice === "paper") {
+        pcChoice = "scissors";
+    } else if (playerChoice === "scissors") {
+        pcChoice = "rock";
+    }
+
+    changePcIcon(pcChoice);
+
+    return pcChoice;
+}
+
+function changePcIcon(pcChoice) {
+    pc.removeClass("fa-question");
+    pc.addClass(`fa-hand-${pcChoice}`);
+}
+
+function lastMultiple(num){
+    let len = playerPattern.length;
+    let lastNum = [];
+
+    for(let i = len - num; i < len; i++){
+        lastNum.push(playerPattern[i]);
+    }
+
+    return lastNum;
 }
 
 function gameLogic(playerChoice, pcChoice) {
